7fe77fe28889e3f42e16d00e211b74de1dfeeb5f
Bas <macbaszii@gmail.com>
2018-11-14 14:16:28 +0700

    Initial Commit

---
+++ FetchFunny/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FetchFunny
+//
+//  Created by Anoochitarom, Kiattisak (Agoda) on 14/11/2561 BE.
+//  Copyright © 2561 Anoochitarom, Kiattisak (Agoda). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
